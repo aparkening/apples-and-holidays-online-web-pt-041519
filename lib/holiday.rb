98cb8dadc5e2a2e3e14fb3d4803b417cf0166be8
Aaron Parkening <aaron.parkening@gmail.com>
2019-04-22 17:12:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,8 +1,7 @@
 require 'pry'
 
-def second_supply_for_fourth_of_july(holiday_hash)
-  # given that holiday_hash looks like this:
-  # {
+
+  # holiday_hash = {
   #   :winter => {
   #     :christmas => ["Lights", "Wreath"],
   #     :new_years => ["Party Hats"]
@@ -17,32 +16,34 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :memorial_day => ["BBQ"]
   #   }
   # }
-  # return the second element in the 4th of July array
+
+
+# Return the second element in the 4th of July array
+def second_supply_for_fourth_of_july(holiday_hash)
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
+# Add the second argument, which is a supply, to BOTH the Christmas AND the New Year's arrays
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter].collect { |holiday, supplies| supplies << supply }
 end
 
-
+# Add supply to the memorial day array
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
+# Add a new holiday and its associated supplies to any season
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
 end
 
+# Return an array of all of the supplies that are used in the winter season
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
-
+  supply_array = []
+  holiday_hash[:winter].each { |holiday, supplies| supply_array << supplies}
+  supply_array.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
